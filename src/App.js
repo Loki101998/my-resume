a2e9e4070d10113a2dd7b5b560d2c4af255d3740
Loki101998 <lokesh@ikshalabs.com>
2022-06-24 15:52:29 +0530

    added company name

--- src/App.js
+++ src/App.js
@@ -56,6 +56,7 @@ const totalnumberofsteps= numberofsteps(days,100)
         </h1>
         <p>
           Name : Lokesh.V <br></br> 
+          Company : Iksha Labs <br></br>
           Phone : 7013525217 <br></br>
           Email : lokesh@ikshalabs.com <br></br>
           About me : Developing novel solutions for persisting problems 
