b0ed03e73efc207a6f4bd0dca129b3545e6a5727
Loki101998 <lokesh@ikshalabs.com>
2022-06-28 19:10:52 +0530

    added navbar

--- src/App.js
+++ src/App.js
@@ -3,30 +3,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import './App.css';
-let d =new Date()
-let h=d.getHours()
-let t;
-let m=d.getMinutes()
-let s=d.getSeconds()
-const m1=m/60
-const s1=s/3600
-const time=h+m1+s1;
-let output;
-if (time > 3 && time <= 9)
-   output="Good Morning"
-else if (time > 9 && time <= 15)
-   output="Good Afternoon"
-else if(time > 15 && time <= 21)
-    output="Good Evening"
-else
-    output="Good Night"
-    if(h<12)
-    t='AM'
-  else 
-  {
-    h=h-12
-    t='PM'
-  }   
+import Navbar from './Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Pages';
+import About from './Pages/about';
+import SignUp from './Pages/signup';
+
 
  function numberofsteps(dayswalked,dailySteps)
  {
@@ -40,34 +22,23 @@ else
  }}
  let days = Math.floor(Math.random() * 100) + 1;
 const totalnumberofsteps= numberofsteps(days,100)
- let b=totalnumberofsteps();
+let b=totalnumberofsteps();
+
  
  function App() {
   return (
     <>
-    <div className="App">
-      <header className="App-header">
-        <img src={process.env.PUBLIC_URL+"Loki.jpg"} /> 
-        <h1>
-         Current Time(IST) : {h}:{m}:{s} {t}
-          </h1> 
-        <h1>
-          {output}
-        </h1>
-        <p>
-          Name : Lokesh.V <br></br> 
-          Company : Iksha Labs <br></br>
-          Location : Gurgaon <br></br>
-          Phone : 7013525217 <br></br>
-          Email : lokesh@ikshalabs.com <br></br>
-          About me : Developing novel solutions for persisting problems 
-        </p>
-        <button type="button">Click Me</button>
-        {/* <button onClick = {clickMe} >click me</button> */}
-        {/* <h1>Total number of steps after {days} days are {b}</h1> */}
-        </header>
-    </div>
-  </>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' exact component={Home} />
+        <Route path='/about' component={About} />
+        <Route path='/sign-up' component={SignUp} />
+      </Routes>
+    </Router>
+    
+    </>
+  
   );
 }
 export default App;
