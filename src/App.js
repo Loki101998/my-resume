c936b1bbd5da078eaff68faf11864e3992e35647
Sheerin Naseem <sheerin.naseem@gmail.com>
2022-06-28 19:26:34 +0530

    Structure change and dependancies updated

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import './App.css';
 import Navbar from './Navbar';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Home from './Pages';
 import About from './Pages/about';
 import SignUp from './Pages/signup';
@@ -28,14 +28,12 @@ let b=totalnumberofsteps();
  function App() {
   return (
     <>
-    <Router>
+    <BrowserRouter>
       <Navbar />
-      <Routes>
         <Route path='/' exact component={Home} />
         <Route path='/about' component={About} />
         <Route path='/sign-up' component={SignUp} />
-      </Routes>
-    </Router>
+    </BrowserRouter>
     
     </>
   
